d987b905b71e1a019c829e6a0d2fb6266b3e3f69
begumdev <ruk90@outlook.com>
2024-01-11 11:04:29 +0000

    Add allauth templates and base.html

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -64,13 +66,14 @@ LOGIN_URL = './accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
 
-
 ROOT_URLCONF = 'ecomm.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join('BASE_DIR', 'templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
